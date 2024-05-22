7ba85f3cc608330a5983516875389424dbc3029d
winnyDiese <winnydiese@gmail.com>
2024-05-22 22:23:46 +0200

    jblhbh

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-    res.send('Home page...')
+    // res.send('Home page...')
+    res.render('index',{title:"Home page"})
 })
 
 module.exports = router
