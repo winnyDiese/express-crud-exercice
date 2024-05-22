5f221cc4316c3d092c28d84e2bd8363d372341c3
winnyDiese <winnydiese@gmail.com>
2024-05-22 20:31:31 +0200

    iubiubiub

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 4000
 
+// database connection
+mongoose.connect(process.env.DB_URL, {useNewParser: true})
+const db = mongoose.connection()
+db.on('error',(error)=>console.log(error))
+db.once('open',()=>console.log('Connected to the database'))
+
+
 app.get('/',(req,res)=>{
     res.send('Hello')
 })
