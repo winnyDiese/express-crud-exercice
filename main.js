f202cab8176c005390e60277b735ed5110c16d12
winnyDiese <winnydiese@gmail.com>
2024-05-22 17:48:26 +0200

    ubbuyuyb

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
+
+app.listen(PORT, ()=>{console.log("Server started http://localhost:"+PORT)})
 
