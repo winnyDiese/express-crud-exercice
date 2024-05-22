b6b1d420c5fdeb0a58a2e181ef9101a202f2f067
winnyDiese <winnydiese@gmail.com>
2024-05-22 22:14:01 +0200

    creating a view...

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/users',(req,res)=>{
-    res.send('All users')
+router.get('/',(req,res)=>{
+    res.send('Home page...')
 })
 
 module.exports = router
