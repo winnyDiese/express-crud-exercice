f4cee389f72389eb922a4668d6c34eaabc427f28
winnyDiese <winnydiese@gmail.com>
2024-05-22 22:35:10 +0200

    view add_user...

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,8 @@ router.get('/',(req,res)=>{
     res.render('index',{title:"Home page"})
 })
 
+router.get('/add',(req,res)=>{
+    res.render('add_user',{title:"Add user"})
+})
+
 module.exports = router
